eb5309042037d346a2e15b760f06bc0e63ccb9f5
murilojcosta <murilojcosta@gmail.com>
2023-11-24 12:11:29 -0300

    new sprite and animation to destination

--- Assets/Scripts/RobotDestination.cs
+++ Assets/Scripts/RobotDestination.cs
@@ -7,10 +7,19 @@ public class RobotDestination : MonoBehaviour
 {
     [SerializeField] private LayerMask layerRobot;
 
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if ((layerRobot.value & (1 << collision.transform.gameObject.layer)) > 0)
         {
+            animator.SetTrigger("Open");
+
             LevelManager.instance.AddRobotInDestination(1);
         }
     }
